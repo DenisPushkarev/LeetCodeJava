313a989e2bd0eef1862324f52dae757110e6bbbe
Denis Pushkarev <dipushkarev@gmail.com>
2023-02-16 10:56:31 +0100

    Solution 412+

--- src/dip/leetcode/solutions/Solution412.java
+++ src/dip/leetcode/solutions/Solution412.java
@@ -1,25 +1,25 @@
 package dip.leetcode.solutions;
 
 import java.util.List;
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Solution412 {
   public List<String> fizzBuzz(int n) {
-    String[] result = new String[n];
+    List<String> result = new ArrayList<String>(n);
     String fizz = "Fizz";
     String buzz = "Buzz";
     String fizz_buzz = "FizzBuzz";
-    for (int i = 0; i < result.length; i++) {
-      String s = Integer.toString(i + 1);
-      if ((i + 1) % 3 == 0 && (i + 1) % 5 == 0) {
-        s = fizz_buzz;
-      } else if ((i + 1) % 3 == 0) {
-        s = fizz;
-      } else if ((i + 1) % 5 == 0) {
-        s = buzz;
+    for (int i = 1; i <= n; i++) {
+      if (i % 3 == 0 && i % 5 == 0) {
+        result.add(fizz_buzz);
+      } else if ((i) % 3 == 0) {
+        result.add(fizz);
+      } else if ((i) % 5 == 0) {
+        result.add(buzz);
+      } else {
+        result.add(Integer.toString(i));
       }
-      result[i] = s;
     }
-    return Arrays.asList(result);
+    return result;
   }
 }
