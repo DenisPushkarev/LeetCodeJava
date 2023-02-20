8b432b281ce3629b3ac54688cd54bbe01f4b8611
Denis Pushkarev <dipushkarev@gmail.com>
2023-02-20 10:26:28 +0100

    Solution 461

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
-
-import dip.leetcode.solutions.Solution191;
 public class Main {
 
     public static void main(String[] args) {
-        Solution191 s191 = new Solution191();
-        int n = 0b11111111111111111111111111111101;
-        System.out.println("n: " + n);
-        System.out.println(s191.hammingWeight(0b11111111111111111111111111111101));
+        int m = 1, n = 20;
+        for (int i = m; i <= n; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
     }
-
 }
